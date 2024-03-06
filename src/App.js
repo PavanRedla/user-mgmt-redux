bf23d25af6ba624ddfe7a9686adceb932ad2b56f
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-03-06 21:51:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserCRUD from './features/user/UserCRUD';
+
+function App() {
+  return (
+    <div className="App">
+      <UserCRUD />
+    </div>
+  );
+}
+
+export default App;
